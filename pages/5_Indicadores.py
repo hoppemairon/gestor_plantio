6818d6c9228568e7704eb7b886ffeffb113b6226
Mairon Hoppe <hoppe.mairon@gmail.com>
2025-07-09 18:07:12 -0300

    feat: add FPDF import for PDF generation functionality

--- pages/5_Indicadores.py
+++ pages/5_Indicadores.py
@@ -4,6 +4,7 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 from io import BytesIO
+from fpdf import FPDF
 import datetime
 
 from utils.session import carregar_configuracoes
